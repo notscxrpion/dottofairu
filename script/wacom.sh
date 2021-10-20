dc15dca79a0c6e71fbceb90a09b7af83af8049e5
notscxrpion <djusupovic@gmail.com>
2021-10-20 15:09:14 +0200

    yes.

---
+++ script/wacom.sh
@@ -0,0 +1,7 @@
+xsetwacom set 'Wacom Intuos PT S Pen stylus' MapToOutput DisplayPort-0
+xsetwacom set 'Wacom Intuos PT S Pen stylus' RawSample 1
+xsetwacom set 'Wacom Intuos PT S Finger touch' TOUCH off
+xsetwacom set 'Wacom Intuos PT S Pen stylus' Button 1 'button 0'
+xsetwacom set 'Wacom Intuos PT S Pen stylus' Button 2 'button 0'
+xsetwacom set 'Wacom Intuos PT S Pen stylus' Button 3 'button 0'
+xsetwacom set "Wacom Intuos PT S Pen stylus" Suppress 0
